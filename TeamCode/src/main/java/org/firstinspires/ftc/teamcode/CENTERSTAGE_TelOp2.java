fa53b439b71547d90e5d05e16759c353280c4c6f
alligatorloki <spencer@robinsoncrew.net>
2023-10-23 13:49:05 -0500

    armlock working minus shaft

---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/CENTERSTAGE_TelOp2.java
@@ -0,0 +1,27 @@
+package org.firstinspires.ftc.teamcode;
+
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.eventloop.opmode.TeleOp;
+import com.qualcomm.robotcore.hardware.Servo;
+import org.firstinspires.ftc.teamcode.MecanumLibrary;
+
+@TeleOp( group = "CENTERSTAGE")
+public class CENTERSTAGE_TelOp2 extends LinearOpMode {
+    private MecanumLibrary mecanum;
+    private MotorHardwareMap motors;
+    @Override
+    public void runOpMode(){
+        mecanum = new MecanumLibrary(hardwareMap, telemetry);
+        motors = new MotorHardwareMap(hardwareMap, telemetry);
+        mecanum.begin();
+        motors.begin();
+        waitForStart();
+        while(opModeIsActive()){
+            mecanum.update(gamepad1.left_stick_x * 1.1, gamepad1.left_stick_y, gamepad1.right_stick_x, gamepad1.left_trigger==1,gamepad1.right_trigger==1,gamepad1.left_bumper&&gamepad1.right_bumper);
+            motors.update(gamepad2.left_stick_y,gamepad2.x);
+        }
+
+    }
+}
---
+++ TeamCode/src/main/java/org/firstinspires/ftc/teamcode/MotorHardwareMap.java
@@ -0,0 +1,37 @@
+package org.firstinspires.ftc.teamcode;
+import com.qualcomm.hardware.bosch.BNO055IMU;
+import com.qualcomm.robotcore.eventloop.opmode.LinearOpMode;
+import com.qualcomm.robotcore.hardware.DcMotor;
+import com.qualcomm.robotcore.hardware.HardwareMap;
+import com.qualcomm.robotcore.hardware.Servo;
+
+import org.firstinspires.ftc.robotcore.external.Telemetry;
+import org.firstinspires.ftc.robotcore.external.navigation.Acceleration;
+import org.firstinspires.ftc.robotcore.external.navigation.Orientation;
+public class MotorHardwareMap {
+    public HardwareMap hardwareMap;
+    public Telemetry telemetry;
+    public DcMotor arm;
+    boolean armlockPosition = false;
+    boolean prevArmToggle = false;
+    public Servo armlock;
+    public MotorHardwareMap (HardwareMap map, Telemetry telem) {
+        telemetry = telem;
+        hardwareMap = map;
+    }
+    public void begin () {
+        arm = hardwareMap.dcMotor.get("arm");
+        arm.setZeroPowerBehavior(DcMotor.ZeroPowerBehavior.BRAKE);
+        armlock = hardwareMap.servo.get("armlock");
+    }
+    public void update (float armPower, boolean armToggle) {
+        arm.setPower((double)armPower);
+        if(armToggle && !prevArmToggle){armlockPosition=!armlockPosition;}
+        if (armlockPosition){armlock.setPosition(1.0);}
+        else {armlock.setPosition(0.7);}
+        //armlock.setPosition(armPower);
+        telemetry.addData("servo: ",armlock.getPosition());
+        telemetry.update();
+        prevArmToggle = armToggle;
+    }
+}
